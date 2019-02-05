9e6ccf9d561c43103744ac1efb10a2631fba01d4
sejinwls <misshufflepuff1@gmail.com>
2019-02-05 15:35:56 +0100

    example 1 for counting As

--- session-5/ex-1.py
+++ session-5/ex-1.py
@@ -1,2 +1,26 @@
 def count_a(seq):
     """"THis function is for counting the number of A's in the sequence"""
+
+    # Counter for the As
+    result = 0
+
+    for b in seq:
+        if b == "A":
+            result += 1
+
+    # Return the result
+    return result
+
+
+# Main program
+s = "AGTACATTGA"
+na = count_a(s)
+print("The number of As is : {}".format(na))
+
+# Calculate the total sequence lenght
+tl = len(s)
+
+# Calculate the percentage of As in the sequence
+perc = round(100.0 * na/tl, 1)
+print("The total lenght is: {}".format(tl))
+print("The percentage of As is {}%".format(perc))

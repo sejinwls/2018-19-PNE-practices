3290ca307d55694e4aa54b5093c8981a37c49c3c
sejinwls <Hillsong7fyq>
2019-02-07 10:45:31 +0100

    Main program where seq class is used

---
+++ P1/seq_main.py
@@ -0,0 +1,15 @@
+from seq import Seq
+
+s1 = Seq("ACGT")
+s2 = Seq("GTCAGTCA")
+s3 = Seq(s1.complement())
+s4 = Seq(s2.reverse())
+
+seqs = [s1, s2, s3 ,s4]
+index = 1
+for seq in seqs:
+    print("""Sequence""", index, """: """,seq.strbases, """
+    Length:{}""".format(s1.len()),"""
+    Bases count:  A:{}""".format(seq.count("A")),"""C:{}""".format(seq.count("C")), """G:{}""".format(seq.count("G")), """T:{}""".format(seq.count("T")),"""
+    Bases percentage: A:{}""".format(seq.perc("A")),"""C:{}""".format(seq.perc("C")), """G:{}""".format(seq.perc("G")), """T:{}""".format(seq.perc("T")))
+    index +=1

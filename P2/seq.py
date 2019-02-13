5499d4a57b1a23781940a4b2e885a34b637ea934
sejinwls <misshufflepuff1@gmail.com>
2019-02-13 17:28:46 +0100

    server and seq class files

---
+++ P2/seq.py
@@ -0,0 +1,32 @@
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        self.strbases = strbases
+
+    def len(self):
+        return len(self.strbases)
+
+    def complement(self):
+        comp = {"A": "T", "T": "A", "C": "G", "G": "C"}
+        comp_seq = ""
+        for b in self.strbases:
+            comp_seq = comp_seq + comp[b]
+        return comp_seq
+
+    def reverse(self):
+        rev_seq = ""
+        for b in self.strbases:
+            rev_seq = b + rev_seq
+        return rev_seq
+
+    def count(self, base):
+        counter = self.strbases.count(base)
+        return counter
+
+    def perc(self, base):
+        tl = len(self.strbases)
+        counter = self.strbases.count(base)
+        perc = 100.0 * counter/tl
+        return perc
+
+

89ef13e3caedc4bbbe75d54b06fd5755b8d1c7ce
sejinwls <misshufflepuff1@gmail.com>
2019-02-19 14:56:53 +0100

    Corrected the reverse/complement

--- P2/client.py
+++ P2/client.py
@@ -3,7 +3,7 @@ import socket
 
 s1 = Seq(input("Please introduce a sequence: "))
 s2 = Seq(s1.reverse())
-s3 = Seq(s1.complement())
+s3 = Seq(s2.complement())
 
 print(s2.strbases)
 print(s3.strbases)
@@ -15,8 +15,7 @@ IP = "212.128.253.73"
 s.connect((IP, PORT))
 
 # Send a message
-s.send(str.encode("Reverse:{}".format(s2.strbases)))
-s.send(str.encode("\nComplement:{}".format(s3.strbases)))
+s.send(str.encode("Reverse/complement:{}".format(s3.strbases)))
 
 
 msg = s.recv(2048).decode("utf-8")

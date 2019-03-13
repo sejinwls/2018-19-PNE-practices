a84d6035cca84b8c802482a97834b0ecc345ded5
sejinwls <Hillsong7fyq>
2019-03-13 12:21:12 +0100

    Session 15 first exercise

--- Session-15/echoserver.py
+++ Session-15/echoserver.py
@@ -11,33 +11,33 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         # -- printing th request line
         termcolor.cprint(self.requestline, 'green')
-
-        f = open('echoform.html', 'r')
-        contents = f.read()
-
-        self.send_response(200)
-
-        self.send_header('Content-Type', 'text.html')
-        self.send_header('Content-Length', len(str.encode(contents)))
-        self.end_headers()
-
-        # --Sending he body of the response message
-        self.wfile.write(str.encode(contents))
-
-        inputs = self.requestline[14:].split(" ")
-        msg = inputs[0]
-        if msg != "":
-            f = open('echoform2.html', 'r')
+        if not("msg" in self.requestline):
+            f = open('echoform.html', 'r')
             contents = f.read()
 
             self.send_response(200)
 
-            self.send_header('Content-Type', 'text/plain')
+            self.send_header('Content-Type', 'text.html')
             self.send_header('Content-Length', len(str.encode(contents)))
             self.end_headers()
 
             # --Sending he body of the response message
             self.wfile.write(str.encode(contents))
+        elif "msg" in self.requestline:
+            inputs = self.requestline[14:].split(" ")
+            msg = inputs[0]
+            if msg != "":
+                f = open('echoform2.html', 'r')
+                contents = f.read()
+
+                self.send_response(200)
+
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Length', len(str.encode(contents)))
+                self.end_headers()
+
+                # --Sending he body of the response message
+                self.wfile.write(str.encode(contents))
 # Main program
 with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
     print("Serving at PORT: {}". format(PORT))

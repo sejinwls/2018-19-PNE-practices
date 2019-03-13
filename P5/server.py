57c830b2cb194150d72e968152330e01cdb6306a
sejinwls <Hillsong7fyq>
2019-03-13 11:54:25 +0100

    Practice 3 with some changes

--- P5/server.py
+++ P5/server.py
@@ -1,2 +1,47 @@
 import http.server
+import socketserver
 
+PORT = 8080
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("GET received")
+
+        print("Request line:" + self.requestline)
+        print("   cmd: " + self.command)
+        print("   Path: " + self.path)
+        if self.requestline.startswith("GET / ") or self.requestline.startswith("GET /index"):
+            f = open('index.html', 'r')
+            content = f.read()
+            f.close()
+        elif self.requestline.startswith("GET /blue"):
+            f = open('blue.html', 'r')
+            content = f.read()
+            f.close()
+        elif self.requestline.startswith("GET /pink"):
+            f = open('pink.html', 'r')
+            content = f.read()
+            f.close()
+        else:
+            f = open('error.html', 'r')
+            content = f.read()
+            f.close()
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-lenght', len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
+
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("192.168.1.48", PORT), Handler) as httpd:
+    print("Serving at port", PORT)
+
+    httpd.serve_forever()

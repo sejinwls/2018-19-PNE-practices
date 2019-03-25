90603a0ae13398afec16201ab64a4c825da1a99b
sejinwls <Hillsong7fyq>
2019-03-25 11:41:21 +0100

    Files from session 17

---
+++ session-17/client2.py
@@ -0,0 +1,60 @@
+# -- Example of a client that uses the HTTP.client library
+# -- for requesting a JSON object and printing their
+# -- contents
+import http.client
+import json
+import termcolor
+
+PORT = 8001
+SERVER = 'localhost'
+
+print("\nConnecting to server: {}:{}\n".format(SERVER, PORT))
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+conn.request("GET", "/listusers")
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Create a variable with the data,
+# -- form the JSON received
+person = json.loads(data1)
+
+print("CONTENT: ")
+
+# Print the information in the object
+print()
+print("Total people in the database: {}".format(len(person["people"])))
+for i, num in enumerate(person["people"]):
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person["people"][i]['Firstname'], person["people"][i]['Lastname'])
+
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person["people"][i]['age'])
+
+    # Get the phoneNumber list
+    phoneNumbers = person["people"][i]['phoneNumber']
+
+    # Print the number of elements int the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    # Print all the numbers
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])

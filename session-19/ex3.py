2a24fd765e64b8dd7b86396c5c6547f8da759e0a
sejinwls <Hillsong7fyq>
2019-03-27 17:45:33 +0100

    Exercise 3

--- session-19/ex3.py
+++ session-19/ex3.py
@@ -43,13 +43,57 @@ conn.close()
 user = json.loads(text_json)
 
 # -- Get some data
-login = user['login']
 name = user['name']
-bio = user['bio']
 nrepos = user['public_repos']
+repos = user['repos_url']
+def get_repos(url):
+    HOSTNAME = "api.github.com"
+    ENDPOINT = url[22:]
+    METHOD = "GET"
 
+    # -- Here we can define special headers if needed
+    headers = {'User-Agent': 'http-client'}
+
+    # -- Connect to the server
+    # -- NOTICE it is an HTTPS connection!
+    # -- If we do not specify the port, the standar one
+    # -- will be used
+    conn = http.client.HTTPSConnection(HOSTNAME)
+
+    # -- Send the request. No body (None)
+    # -- Use the defined headers
+    conn.request(METHOD, ENDPOINT, None, headers)
+
+    # -- Wait for the server's response
+    r1 = conn.getresponse()
+
+    # -- Print the status
+    print()
+    print("Response received: ", end='')
+    print(r1.status, r1.reason)
+
+    # -- Read the response's body and close
+    # -- the connection
+    text_json = r1.read().decode("utf-8")
+    conn.close()
+
+    # -- Optionally you can print the
+    # -- received json file for testing
+    # print(text_json)
+
+    # -- Generate the object from the json file
+    repos_info = json.loads(text_json)
+    names = []
+    number_commits = len(repos_info[0]['commits_url'])
+    for repository in repos_info:
+        names.append(repository['name'])
+    names_list = ''
+    for name in names:
+        names_list = names_list + name + ', '
+    return names_list, number_commits
+
+info, commits = get_repos(repos)
 print()
-print("User: {}".format(login))
 print("Name: {}".format(name))
-print("Repos: {}".format(nrepos))
-print("Bio: \n{}".format(bio))
+print("Repos: {}".format(info))
+print("Number of commits to 2018-19-PNE-practices: {}".format(commits))
